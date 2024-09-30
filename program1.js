774462c01536e78908979deb1cbb045208e22984
Himanshichaturvedi <himanshichaturvedi950@gmail.com>
2024-09-30 14:43:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const pairs = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+    };
+
+    for (let char of str) {
+        if (pairs[char]) {
+            stack.push(char);
+        } else if (
+            char === ")" ||
+            char === "]" ||
+            char === "}"
+        ) {
+            if (
+                pairs[stack.pop()] !==
+                char
+            ) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+
+
 };
 
 module.exports = { isValid };
